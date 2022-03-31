b22dd5df4a4087445e81a08be8fdf80814d7ded3
Knob <59198478+nbr41to@users.noreply.github.com>
2022-03-31 19:08:04 +0900

    .

--- src/components/@base/Modal.tsx
+++ src/components/@base/Modal.tsx
@@ -9,7 +9,7 @@ export const Modal: FC<Props> = ({ isOpen, closeHandler, children }) => {
   if (isOpen) {
     return (
       <div className='fixed inset-0 z-50 bg-gray-900/50 backdrop-blur'>
-        <div className='absolute z-50 w-full h-full flex justify-center items-center cursor-pointer'>
+        <div className='absolute z-50 w-full h-full flex justify-center items-center'>
           <div className='relative z-50 shadow'>{children}</div>
           {/* Overlay */}
           <div
